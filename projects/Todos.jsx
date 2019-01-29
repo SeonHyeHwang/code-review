f6ddc640790d5ed0c1ab9629df13f388ede44de6
hsh1209 <shaina1209@gmail.com>
2019-01-29 16:11:55 +0900

    Todos commit

---
+++ projects/Todos.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Todos.css'
+
+const Todos = (props) => {
+    return (
+      <div className ="Todos">
+        <h2>{props.title}</h2>
+        <ul>
+          {props.items.map((item, index)=>{
+            return <li key={index}><input type="checkbox" />{item}</li>
+          })}
+        </ul>
+      </div>
+    );
+}
+
+export function x () {
+  return 1;
+}
+export default Todos;
