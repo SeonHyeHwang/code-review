5889677a4c533a16c0c0df0aead1e86f235320a5
hsh1209 <shaina1209@gmail.com>
2019-01-29 17:12:42 +0900

    todos 커밋

--- projects/Todos.jsx
+++ projects/Todos.jsx
@@ -3,18 +3,18 @@ import './Todos.css'
 
 const Todos = (props) => {
     return (
-      <div className ="Todos">
-        <h2>{props.title}</h2>
-        <ul>
-          {props.items.map((item, index)=>{
-            return <li key={index}><input type="checkbox" />{item}</li>
-          })}
-        </ul>
-      </div>
+        <div className ="Todos">
+            <h2>{props.title}</h2>
+            <ul>
+                {props.items.map((item, index)=>{
+                    return <li key={index}><input type="checkbox" checked={item.completed}/>{item.name}</li>
+                })}
+            </ul>
+        </div>
     );
 }
 
 export function x () {
-  return 1;
+    return 1;
 }
 export default Todos;
